18dc85fafaf29997f47d7779a439a6dd679d9fb9
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2024-08-27 11:10:15 +0200

    docs: remove useless lines in dashboard script

--- .github/scripts/dashboard_update_repos.sh
+++ .github/scripts/dashboard_update_repos.sh
@@ -7,13 +7,6 @@ mkdir dashboard/
 
 echo "{ \"datetime\":\"`date --utc --iso-8601=seconds`\"}" > dashboard/datetime.json
 
-keypop-reader-java-api
-keypop-card-java-api
-keypop-calypso-card-java-api
-keypop-calypso-crypto-legacysam-java-api
-keypop-calypso-crypto-symmetric-java-api
-keypop-calypso-crypto-asymmetric-java-api
-
 echo "[" >> dashboard/repository_list.json
 ../.github/scripts/dashboard_update_repo.sh $token keypop-reader-java-api main true true
 echo "," >> dashboard/repository_list.json
