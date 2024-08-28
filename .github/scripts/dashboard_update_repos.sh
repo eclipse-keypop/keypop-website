884336604b3f93d6c9d84f2f4ab64ebc637ad975
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2024-08-28 10:29:51 +0200

    docs: add c++ projects to dashboard (#7)

--- .github/scripts/dashboard_update_repos.sh
+++ .github/scripts/dashboard_update_repos.sh
@@ -7,13 +7,6 @@ mkdir dashboard/
 
 echo "{ \"datetime\":\"`date --utc --iso-8601=seconds`\"}" > dashboard/datetime.json
 
-keypop-reader-java-api
-keypop-card-java-api
-keypop-calypso-card-java-api
-keypop-calypso-crypto-legacysam-java-api
-keypop-calypso-crypto-symmetric-java-api
-keypop-calypso-crypto-asymmetric-java-api
-
 echo "[" >> dashboard/repository_list.json
 ../.github/scripts/dashboard_update_repo.sh $token keypop-reader-java-api main true true
 echo "," >> dashboard/repository_list.json
@@ -26,6 +19,18 @@ echo "," >> dashboard/repository_list.json
 ../.github/scripts/dashboard_update_repo.sh $token keypop-calypso-crypto-symmetric-java-api main true true
 echo "," >> dashboard/repository_list.json
 ../.github/scripts/dashboard_update_repo.sh $token keypop-calypso-crypto-asymmetric-java-api main true true
+echo "," >> dashboard/repository_list.json
+../.github/scripts/dashboard_update_repo.sh $token keypop-reader-cpp-api main true true
+echo "," >> dashboard/repository_list.json
+../.github/scripts/dashboard_update_repo.sh $token keypop-card-cpp-api main true true
+echo "," >> dashboard/repository_list.json
+../.github/scripts/dashboard_update_repo.sh $token keypop-calypso-card-cpp-api main true true
+echo "," >> dashboard/repository_list.json
+../.github/scripts/dashboard_update_repo.sh $token keypop-calypso-crypto-legacysam-cpp-api main true true
+echo "," >> dashboard/repository_list.json
+../.github/scripts/dashboard_update_repo.sh $token keypop-calypso-crypto-symmetric-cpp-api main true true
+echo "," >> dashboard/repository_list.json
+../.github/scripts/dashboard_update_repo.sh $token keypop-calypso-crypto-asymmetric-cpp-api main true true
 echo "," >> dashboard/repository_list.json
 ../.github/scripts/dashboard_update_repo.sh $token keypop-website main false false
 echo "" >> dashboard/repository_list.json
